b832d04e35536350ceeac127278c3eb76cd09f97
Joe Simon <simonjm09@gmail.com>
2016-07-11 20:13:37 -0500

    Only convert audio if necessary. Added better error messages

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"golang.org/x/exp/inotify"
@@ -106,9 +110,22 @@ func convertFiles(videosChan <-chan string) {
 	for video := range videosChan {
 		mp4File := strings.Replace(video, ".mkv", ".mp4", 1)
 		output := filepath.Join(*outDir, filepath.Base(mp4File))
-		logger.Printf("Converting %s to %s\n", video, output)
-		cmd := exec.Command("/usr/bin/ffmpeg", "-i", video, "-c:v", "copy", "-c:a", "aac", "-b:a", "192k", output)
-		if err := cmd.Run(); err != nil {
+
+		commandArgs := []string{"-y", "-i", video, "-c:v", "copy", "-c:a"}
+		shouldConvert, err := shouldConvertAudio(video)
+		if err != nil {
+			logger.Println(err)
+			continue
+		}
+
+		if shouldConvert {
+			commandArgs = append(commandArgs, "aac", "-b:a", "192k", output)
+		} else {
+			commandArgs = append(commandArgs, "copy", output)
+		}
+
+		logger.Printf("Running ffmpeg with arguments %v", commandArgs)
+		if _, err := runCommandWithErrorMsg("/usr/bin/ffmpeg", commandArgs...); err != nil {
 			logger.Println(err)
 			continue
 		}
@@ -120,4 +137,60 @@ func convertFiles(videosChan <-chan string) {
 
 		logger.Printf("Finished %s\n", output)
 	}
+}
+
+// Runs the command and returns the stdout or an error with stderr as the message
+func runCommandWithErrorMsg(command string, args ...string) (string, error) {
+	cmd := exec.Command(command, args...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+
+	outBuf := bytes.Buffer{}
+	if _, err := io.Copy(&outBuf, stdout); err != nil {
+		return "", err
+	}
+
+	errBuf := bytes.Buffer{}
+	if _, err := io.Copy(&errBuf, stderr); err != nil {
+		return "", err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return "", fmt.Errorf("%s. Stderr - %s", err, errBuf.String())
+	}
+
+	return outBuf.String(), nil
+}
+
+// Checks if video needs the audio converted to AAC
+func shouldConvertAudio(filename string) (bool, error) {
+	probeCommand := fmt.Sprintf("ffprobe -v error -show_format -show_streams \"%s\" | grep codec_name", filename)
+	output, err := runCommandWithErrorMsg("/bin/sh", "-c", probeCommand)
+	if err != nil {
+		return false, err
+	}
+
+	lines := strings.Split(string(output), "\n")
+	for _, l := range lines {
+		matched, err := regexp.Match("codec_name=aac", []byte(l))
+		if err != nil {
+			return false, err
+		}
+
+		if matched {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
