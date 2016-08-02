620c981e972eb2aff17ee83a0f336b8c59bc9f47
Joe Simon <simonjm09@gmail.com>
2016-08-01 19:21:56 -0500

    Updated deploy script for new raspberry pi

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-GOOS=linux GOARCH=arm GOARM=6 go build
-scp hawkeye joe@192.168.11.2:/home/joe/bin
+GOOS=linux GOARCH=arm GOARM=7 go build
+scp hawkeye joe@192.168.11.16:/home/joe/bin
 rm -f hawkeye
